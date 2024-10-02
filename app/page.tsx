21c693a2943edaa5d71f6c13a02970c8012a200c
TomaHara <pyonsuke0720@gmail.com>
2024-10-02 17:09:53 +0900

    🐛 縦幅のレスポンシブの修正

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <Fragment>
       <main
-        className="w-full sm:h-screen h-svh overflow-y-auto relative flex flex-col items-center justify-start bg-fixed"
+        className="w-full  relative flex flex-col items-center justify-start bg-fixed"
         style={{ backgroundImage: "url(/white_back.jpg)" }}
       >
         <WelcomePage />
