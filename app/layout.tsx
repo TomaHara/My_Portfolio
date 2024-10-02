e0cc50d854ed6895a694eacbddd7ee091d4fb0db
TomaHara <pyonsuke0720@gmail.com>
2024-10-02 18:30:19 +0900

    背景の修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main
-          className="fixed w-full h-screen bg-fixed -z-10"
+          className="fixed top-0 left-0 w-full h-screen bg-fixed -z-10"
           style={{ backgroundImage: "url(/white_back.jpg)" }}
         ></main>
         <Header />
